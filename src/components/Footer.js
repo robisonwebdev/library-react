7ca45f16745e8f21471496ed86a7890057a645f7
David Robison <david@robisonweb.dev>
2021-05-12 17:58:33 -0600

    Created Footer function with props. Called props.name between 'a' element. Exported Footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+    return (
+        <div id='footer'>
+            <small>Developed by <a href='#' target='_blank'>{props.name}</a></small>
+        </div>
+    );
+}
+
+export default Footer;
