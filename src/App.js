39e2969206ac69398d6e1b49b831bfdcd8d0407e
David Robison <david@robisonweb.dev>
2021-05-12 18:06:56 -0600

    Imported and added Main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <div>
       <Header title={'Library'} />
+      <Main />
       <Footer name={'David Robison'} />
     </div>
   );
