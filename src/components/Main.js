37d27c4ffa9f9f257790010ad0fec8b31068db35
David Robison <david@robisonweb.dev>
2021-05-14 12:08:56 -0600

    Imported and called Library component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import Button from "./Button"
+import Library from "./Library";
 
 const Main = (props) => {
     return (
         <div>
+            <Library />
             <Button name={'Add Book'} />
         </div>
     );
