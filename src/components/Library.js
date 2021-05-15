e3f3d37c2406b772805447caab266dd04e55a177
David Robison <david@robisonweb.dev>
2021-05-15 09:08:52 -0600

    Added table with thead and th's

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,16 @@
 const Library = (props) => {
     return (
-        <div>Library Div</div>
+        <div>
+            <table>
+                <thead>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Pages</th>
+                    <th>Status</th>
+                    <th>Del</th>
+                </thead>
+            </table>
+        </div>
     );
 }
 
