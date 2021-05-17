7352a9068827dc9633752d06e27187648c708cf2
David Robison <david@robisonweb.dev>
2021-05-16 20:16:58 -0600

    Setup five test td's

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,7 +1,11 @@
 const BookDetails = (props) => {
     return (
         <div className='bookDetails'>
-
+            <td>Test 1</td>
+            <td>Test 2</td>
+            <td>Test 3</td>
+            <td>Test 4</td>
+            <td>Test 5</td>
         </div>
     );
 }
