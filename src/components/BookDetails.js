a2d3770638a0a01dfc017286ea65e908acad18fa
David Robison <david@robisonweb.dev>
2021-05-16 20:30:22 -0600

    Changed div to tr

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,12 +1,12 @@
 const BookDetails = (props) => {
     return (
-        <div className='bookDetails'>
+        <tr className='bookDetails'>
             <td>Test 1</td>
             <td>Test 2</td>
             <td>Test 3</td>
             <td>Test 4</td>
             <td>Test 5</td>
-        </div>
+        </tr>
     );
 }
 
