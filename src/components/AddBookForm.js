99365ea48a93d2da12d0fc63d6d5a143e7d6e2f6
David Robison <david@robisonweb.dev>
2021-05-18 12:21:04 -0600

    Imported BookFormInputs. Called BookFormInputs.

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,9 +1,12 @@
 import '../styles/AddBookForm.css';
+import BookFormInputs from './BookFormInputs';
 
 const AddBookForm = () => {
     return (
         <form id='addBookForm'>
             <h3>New Book</h3>
+
+            <BookFormInputs />
 
             <div className='bookFormInputs'>
                 <label for='authorInput'>Author</label>
