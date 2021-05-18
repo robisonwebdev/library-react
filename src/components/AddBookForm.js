b08f05f4f6eab522ea58f88af5a286f114bc4e6d
David Robison <david@robisonweb.dev>
2021-05-18 12:22:54 -0600

    Added props to BookFormInputs component

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -6,7 +6,7 @@ const AddBookForm = () => {
         <form id='addBookForm'>
             <h3>New Book</h3>
 
-            <BookFormInputs />
+            <BookFormInputs type={'text'} name={'authorInput'} placeholder={"Enter Author's Name"} />
 
             <div className='bookFormInputs'>
                 <label for='authorInput'>Author</label>
