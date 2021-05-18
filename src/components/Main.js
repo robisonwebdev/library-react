0e4eda954f41f7b288cf79501466d4abcc0a11f9
David Robison <david@robisonweb.dev>
2021-05-17 18:49:50 -0600

    Setup useState. Imported useState

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,11 @@
 import Button from "./Button"
 import Library from "./Library";
 import '../styles/main.css';
+import { useState } from "react";
 
 const Main = (props) => {
+    const [libraryArray, setLibraryArray] = useState([]);
+
     return (
         <div id='main'>
             <Library />
