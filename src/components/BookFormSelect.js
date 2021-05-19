972cde1eaccb11a7885a88f6bf3f524f33632b96
David Robison <david@robisonweb.dev>
2021-05-19 11:59:10 -0600

    Added 3 option values.

--- src/components/BookFormSelect.js
+++ src/components/BookFormSelect.js
@@ -4,6 +4,9 @@ const BookFormSelect = (props) => {
             <label htmlFor={props.name}>{props.label}</label>
             <select name={props.name} id={props.name}>
                 <option value='' selected disabled hidden>None</option>
+                <option value='Read'>Read</option>
+                <option value='Reading'>Currently Reading</option>
+                <option value='Not Read'>Not Read</option>
             </select>
         </div>
     );
