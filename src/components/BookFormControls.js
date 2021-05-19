36811b47da588ffff8e64c0bb8c890ee7fbb168b
David Robison <david@robisonweb.dev>
2021-05-19 12:08:45 -0600

    Called Button component and passed down props. Removed props.

--- src/components/BookFormControls.js
+++ src/components/BookFormControls.js
@@ -1,9 +1,10 @@
 import Button from "./Button"
 
-const BookFormControls = (props) => {
+const BookFormControls = () => {
     return (
         <div className='bookFormControls'>
             <Button class={'bookFormButtons'} name={'Submit'} />
+            <Button class={'bookFormButtons'} name={'Cancel'} />
         </div>
     );
 }
