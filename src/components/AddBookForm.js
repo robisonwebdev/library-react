c72efdd0ee6e44d6715106314ae0c0398cf62c25
David Robison <david@robisonweb.dev>
2021-05-20 10:49:54 -0600

    Moved form into div

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -5,15 +5,17 @@ import BookFormSelect from './BookFormSelect';
 
 const AddBookForm = () => {
     return (
-        <form id='addBookForm'>
-            <h3>New Book</h3>
+        <div>
+            <form id='addBookForm'>
+                <h3>New Book</h3>
 
-            <BookFormInputs label={'Author'} type={'text'} name={'authorInput'} placeholder={"Enter Author's Name"} />
-            <BookFormInputs label={'Title'} type={'text'} name={'titleInput'} placeholder={"Enter Book Title"} />
-            <BookFormInputs label={'Pages'} type={'number'} name={'pagesInput'} placeholder={"Number of Pages"} />
-            <BookFormSelect label={'Status:'} name={'readStatus'} />
-            <BookFormControls />
-        </form>
+                <BookFormInputs label={'Author'} type={'text'} name={'authorInput'} placeholder={"Enter Author's Name"} />
+                <BookFormInputs label={'Title'} type={'text'} name={'titleInput'} placeholder={"Enter Book Title"} />
+                <BookFormInputs label={'Pages'} type={'number'} name={'pagesInput'} placeholder={"Number of Pages"} />
+                <BookFormSelect label={'Status:'} name={'readStatus'} />
+                <BookFormControls />
+            </form>
+        </div>
     );
 }
 
