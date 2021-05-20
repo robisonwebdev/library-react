3acf19f6e51333c1a5f590803b261eb3bd3caf11
David Robison <david@robisonweb.dev>
2021-05-20 10:37:14 -0600

    Imported useState. Added value on select and set to selectValue. Changed None to 'Select One'

--- src/components/BookFormSelect.js
+++ src/components/BookFormSelect.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 const BookFormSelect = (props) => {
+    const [selectValue, setSelectValue] = useState('Select One')
     return (
         <div className='bookFormSelect'>
             <label htmlFor={props.name}>{props.label}</label>
-            <select name={props.name} id={props.name}>
-                <option value='' selected disabled hidden>None</option>
+            <select value={selectValue} name={props.name} id={props.name} readOnly>
+                <option value='Select One' disabled hidden>Select One</option>
                 <option value='Read'>Read</option>
                 <option value='Reading'>Currently Reading</option>
                 <option value='Not Read'>Not Read</option>
