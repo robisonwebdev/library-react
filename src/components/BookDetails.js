ac75abb386837f2e668b55ee5b96fa35e04fe24e
David Robison <david@robisonweb.dev>
2021-05-21 10:59:43 -0600

    Set Button component between <td> element. Gave Button component className of bookStatus. Gave Button component name of props.status.

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,6 +4,7 @@ const BookDetails = (props) => {
             <td>{props.title}</td>
             <td>{props.author}</td>
             <td>{props.pages}</td>
+            <td><Button className={'bookStatus'} name={props.status} /></td>
         </tr>
     );
 }
