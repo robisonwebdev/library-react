235380375063521d317a13651ed37f5783404bb3
David Robison <david@robisonweb.dev>
2021-05-24 09:29:15 -0600

    Add props. Added type props on both Button components. Passed props.event down click prop.

--- src/components/BookFormControls.js
+++ src/components/BookFormControls.js
@@ -1,10 +1,10 @@
 import Button from "./Button"
 
-const BookFormControls = () => {
+const BookFormControls = (props) => {
     return (
         <div className='bookFormControls'>
-            <Button class={'bookFormButtons'} name={'Submit'} />
-            <Button class={'bookFormButtons'} name={'Cancel'} />
+            <Button class={'bookFormButtons'} name={'Submit'} type={'button'} />
+            <Button class={'bookFormButtons'} name={'Cancel'} type={'button'} click={props.events} />
         </div>
     );
 }
