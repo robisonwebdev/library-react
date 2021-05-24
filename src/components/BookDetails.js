c6f08f29f435ffdb0d63a30198952686deb95a3a
David Robison <david@robisonweb.dev>
2021-05-23 20:49:24 -0600

    Fixed typo

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -7,7 +7,7 @@ const BookDetails = (props) => {
             <td>{props.title}</td>
             <td>{props.author}</td>
             <td>{props.pages}</td>
-            <td><Button className={'bookStatus'} name={props.status} /></td>
+            <td><Button class={'bookStatus'} name={props.status} /></td>
             <td><DeleteButton /></td>
         </tr>
     );
