50448ca23758ad299ae731b7204e5b9f72e54d62
David Robison <david@robisonweb.dev>
2021-05-25 09:37:59 -0600

    Passed props.update into click prop

--- src/components/BookFormControls.js
+++ src/components/BookFormControls.js
@@ -3,7 +3,7 @@ import Button from "./Button"
 const BookFormControls = (props) => {
     return (
         <div className='bookFormControls'>
-            <Button class={'bookFormButtons'} name={'Submit'} type={'button'} />
+            <Button class={'bookFormButtons'} name={'Submit'} type={'button'} click={props.update} />
             <Button class={'bookFormButtons'} name={'Cancel'} type={'button'} click={props.events} />
         </div>
     );
