9eca0a9e8b59e6df39a48f759365f796a6b5edc2
David Robison <david@robisonweb.dev>
2021-05-26 09:48:04 -0600

    Created handleChange function to get select value on change and update selectValue state.

--- src/components/BookFormSelect.js
+++ src/components/BookFormSelect.js
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 const BookFormSelect = (props) => {
     const [selectValue, setSelectValue] = useState('Select One');
-    
+
+    function handleChange(e) {
+        setSelectValue(e.target.value);
+    }
+
     return (
         <div className='bookFormSelect'>
             <label htmlFor={props.name}>{props.label}</label>
-            <select value={selectValue} name={props.name} id={props.name} readOnly>
+            <select value={selectValue} onChange={handleChange} name={props.name} id={props.name} readOnly>
                 <option value='Select One' disabled hidden>Select One</option>
                 <option value='Read'>Read</option>
                 <option value='Reading'>Currently Reading</option>
