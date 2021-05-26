18c2da76cae38663c897304ca353f620fb2bd0be
David Robison <david@robisonweb.dev>
2021-05-26 10:04:01 -0600

    Set required to true

--- src/components/BookFormInputs.js
+++ src/components/BookFormInputs.js
@@ -2,7 +2,7 @@ const BookFormInputs = (props) => {
     return (
         <div className='bookFormInputs'>
             <label htmlFor={props.name}>{props.label}</label>
-            <input type={props.type} name={props.name} id={props.name} placeholder={props.placeholder} required />
+            <input type={props.type} name={props.name} id={props.name} placeholder={props.placeholder} required={true} />
         </div>
     );
 }
